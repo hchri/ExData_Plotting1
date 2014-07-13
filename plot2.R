d1b90fc6fea70062c4780da2130a95c7edf8da49
Henry Christensen <henry_christensen@hotmail.com>
2014-07-13 12:13:52 +0200

    1

---
+++ plot2.R
@@ -0,0 +1,25 @@
+household_power_consumption <- read.csv("D:/coursera/ExData/ExData_Plotting1/household_power_consumption.txt", sep=";", quote="", stringsAsFactors=FALSE)
+
+library(lubridate)
+
+
+str(household_power_consumption)
+household_power_consumption$Date <- dmy(household_power_consumption$Date)
+
+household_power_consumption <- subset( household_power_consumption, as.Date(Date) >= as.Date("2007-02-01") & as.Date(Date) <= as.Date("2007-02-02"))
+
+household_power_consumption$Time <- hms(household_power_consumption$Time)
+
+household_power_consumption$Global_active_power <- as.numeric(household_power_consumption$Global_active_power)
+household_power_consumption$Global_reactive_power <- as.numeric(household_power_consumption$Global_reactive_power)
+household_power_consumption$Voltage <- as.numeric(household_power_consumption$Voltage)
+household_power_consumption$Global_intensity <- as.numeric(household_power_consumption$Global_intensity)
+household_power_consumption$Sub_metering_1 <- as.numeric(household_power_consumption$Sub_metering_1)
+household_power_consumption$Sub_metering_2 <- as.numeric(household_power_consumption$Sub_metering_2)
+household_power_consumption$Sub_metering_3 <- as.numeric(household_power_consumption$Sub_metering_3)
+
+
+png(filename = "figure/plot2.png")
+Sys.setlocale("LC_TIME", "UK")
+plot(household_power_consumption$Date + household_power_consumption$Time, household_power_consumption$Global_active_power, type = "l", xlab = NA, ylab = "Global Active Power (kilowatts)")
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,40 @@
+
+
+Sys.setenv(LANGUAGE="en")
+household_power_consumption <- read.csv("D:/coursera/ExData/ExData_Plotting1/household_power_consumption.txt", sep=";", quote="", stringsAsFactors=FALSE)
+
+library(lubridate)
+
+
+str(household_power_consumption)
+household_power_consumption$Date <- dmy(household_power_consumption$Date)
+
+household_power_consumption <- subset( household_power_consumption, as.Date(Date) >= as.Date("2007-02-01") & as.Date(Date) <= as.Date("2007-02-02"))
+
+household_power_consumption$Time <- hms(household_power_consumption$Time)
+
+household_power_consumption$Global_active_power <- as.numeric(household_power_consumption$Global_active_power)
+household_power_consumption$Global_reactive_power <- as.numeric(household_power_consumption$Global_reactive_power)
+household_power_consumption$Voltage <- as.numeric(household_power_consumption$Voltage)
+household_power_consumption$Global_intensity <- as.numeric(household_power_consumption$Global_intensity)
+household_power_consumption$Sub_metering_1 <- as.numeric(household_power_consumption$Sub_metering_1)
+household_power_consumption$Sub_metering_2 <- as.numeric(household_power_consumption$Sub_metering_2)
+household_power_consumption$Sub_metering_3 <- as.numeric(household_power_consumption$Sub_metering_3)
+
+
+png(filename = "figure/plot4.png")
+opar <- par()
+par( mfrow = c(2,2), mar =  c(5, 4, 4, 2) + 0.1)
+Sys.setlocale("LC_TIME", "UK")
+plot(household_power_consumption$Date + household_power_consumption$Time, household_power_consumption$Global_active_power, type = "l", xlab = NA, ylab = "Global Active Power (kilowatts)")
+
+plot(household_power_consumption$Date + household_power_consumption$Time, household_power_consumption$Voltage, type = "l", xlab = "datetime", ylab = "Voltage")
+
+plot(household_power_consumption$Date + household_power_consumption$Time, household_power_consumption$Sub_metering_1,type = "l", xlab = NA, ylab = "Energy sub metering")
+lines(household_power_consumption$Date + household_power_consumption$Time, household_power_consumption$Sub_metering_2, col = "red")
+lines(household_power_consumption$Date + household_power_consumption$Time, household_power_consumption$Sub_metering_3, col = "blue")
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lty = 1 )
+
+plot(household_power_consumption$Date + household_power_consumption$Time, household_power_consumption$Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global_reactive_power")
+par(opar)
+dev.off()
